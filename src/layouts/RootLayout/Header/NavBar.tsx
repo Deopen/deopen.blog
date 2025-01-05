e7d26f491068a7fdb804d9ffc6c90c7f786be201
Omid Yaghoubi <3817604+Deopen@users.noreply.github.com>
2025-01-05 20:47:18 +0100

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  //const links = [{ id: 1, name: "", to: "/#" }]
+  const links = [{ id: 1, name: "", to: "/#" }]
   return (
     <StyledWrapper className="">
       <ul>
